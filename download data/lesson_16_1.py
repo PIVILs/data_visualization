a680b7fe7af335c6cad0fb7b5813ad1c25e55f1a
PIVILs <PIVILs@mail.ru>
2022-02-21 17:21:04 +0400

    add lesson_16_1

--- download data/lesson_16_1.py
+++ download data/lesson_16_1.py
@@ -9,7 +9,6 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
