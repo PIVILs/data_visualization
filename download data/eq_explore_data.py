b21920009a7bab6d6aaf2ae16c61bfe4709c8202
PIVILs <PIVILs@mail.ru>
2022-02-27 15:48:55 +0400

    refactoring all_eq_dicts cycles

--- download data/eq_explore_data.py
+++ download data/eq_explore_data.py
@@ -11,14 +11,10 @@ with open(filename) as f:
 all_eq_dicts = all_eq_data['features']
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 # Нанесение данных на карту.
 data = [{
@@ -35,14 +31,10 @@ data = [{
     },
 }]
 
-my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title=all_eq_data['metadata']['title'])
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
-
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
 
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
